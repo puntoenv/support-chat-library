0e3b9f788890e4d996d4ccbc39942a2aa721328b
puntoenv <sebssjb@gmail.com>
2025-05-04 16:41:00 -0500

    chore: initial project setup with React, TypeScript, and Tailwind CSS for support chat library

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hola Mundo</h1>
+      <Button variant="default">Click me</Button>
+    </>
+  );
+}
+
+export default App;
